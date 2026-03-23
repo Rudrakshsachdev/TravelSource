4158b143da61df6a3800533ec7e544c3dec210c2
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-23 12:31:11 +0530

    feat: fixed core not found error

--- backend/backend/core/serializers/gallery_serializer.py
+++ backend/backend/core/serializers/gallery_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import TripGalleryImage
+from backend.backend.core.models import TripGalleryImage
 
 class TripGalleryImageSerializer(serializers.ModelSerializer):
     class Meta:
